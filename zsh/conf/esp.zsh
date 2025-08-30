f1a234f02aac5db3e2aa9ca49aa95b022cec8211
fard-draf <fard@ik.me>
2025-08-30 21:32:18 +0200

    feat(esp): fix path

--- zsh/conf/esp.zsh
+++ zsh/conf/esp.zsh
@@ -1,6 +1,6 @@
 # ======= CONFIGURATION ESP-IDF & RUST =======
 # Charge la configuration de l'environnement générée par espup.
 # Ce script contient toutes les variables (IDF_PATH, etc.) nécessaires.
-if [ -f "/mnt/repo/dev/tools/esp-idf/export-esp.sh" ]; then
-    source "/mnt/repo/dev/tools/esp-idf/export-esp.sh"
+if [ -f "$DEV_PATH/tools/esp-idf/export-esp.sh" ]; then
+    source "$DEV_PATH/tools/esp-idf/export-esp.sh"
 fi
