593963bfe0c1f5388d85c2966b5629fd9249fd48
fard-draf <fard@ik.me>
2025-09-02 11:03:05 +0200

    update aliases

--- zsh/conf/git.zsh
+++ zsh/conf/git.zsh
@@ -1,19 +1,19 @@
 
 # ======= ALIAS GIT =======
-alias gt='git'
-alias gts='git status'
-alias gta='git add'
-alias gtaa='git add --all'
-alias gtc='git commit -m'
-alias gtp='git push'
-alias gtl='git pull'
-alias gtco='git checkout'
-alias gtcb='git checkout -b'
-alias gtb='git branch'
-alias gtd='git diff'
-alias gtlog='git log --oneline --graph --decorate'
-alias gtnah='git reset --hard && git clean -df'  # Annule tous les changements locaux
-alias gtundo='git reset HEAD~1'                 # Annule le dernier commit
-alias gtsync='git pull origin $(git rev-parse --abbrev-ref HEAD)' # Synchro avec remote
+alias g='git'
+alias gs='git status'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit -m'
+alias gp='git push'
+alias gl='git pull'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gb='git branch'
+alias gd='git diff'
+alias glog='git log --oneline --graph --decorate'
+alias gnah='git reset --hard && git clean -df'  # Annule tous les changements locaux
+alias gundo='git reset HEAD~1'                 # Annule le dernier commit
+alias gsync='git pull origin $(git rev-parse --abbrev-ref HEAD)' # Synchro avec remote
 
 alias git-al="show_aliases_from_file $DOTFILES/shell/zsh/conf/git.zsh"
